51e0492f31262958c1dd603ead6bc089712acd15
zbentzinger <bentzingerz@gmail.com>
2018-11-26 20:24:04 -0700

    Clean up CSS and add MonthSubView.fxml.

--- PersonalPlanner/src/personalplanner/CalendarViewController.java
+++ PersonalPlanner/src/personalplanner/CalendarViewController.java
@@ -1,20 +1,27 @@
 package personalplanner;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
 
 public class CalendarViewController implements Initializable {
 
-    
+
     @FXML private ToggleButton monthButton;
     @FXML private ToggleButton weekButton;
     @FXML private ToggleButton newAppButton;
-    @FXML final ToggleGroup calendarToggleButtons = new ToggleGroup();
+    final ToggleGroup calendarToggleButtons = new ToggleGroup();
+    @FXML
+    private AnchorPane subContentPane;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -24,6 +31,19 @@ public class CalendarViewController implements Initializable {
         newAppButton.setToggleGroup(calendarToggleButtons);
 
         monthButton.setSelected(true);
+
+        AnchorPane subPane;
+        try {
+
+            subPane = FXMLLoader.load(getClass().getResource("MonthSubView.fxml"));
+            subContentPane.getChildren().add(subPane);
+
+
+        } catch (IOException ex) {
+
+            Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
 
     }    
 
@@ -40,5 +60,16 @@ public class CalendarViewController implements Initializable {
     private void newAppButtonClicked(ActionEvent event) {
     }
 
+    private void unloadContentView() {
+
+        try {
+
+            // Unload the previous view from AnchorPane if necessary.
+            subContentPane.getChildren().remove(0);
+
+        } catch (IndexOutOfBoundsException e) {
+        }
+
+    }
     
 }
